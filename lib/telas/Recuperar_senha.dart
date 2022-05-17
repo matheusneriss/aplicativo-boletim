ea5c52add9c00335f99204be165f0309221ea6ff
Matheus Neris <matheusneris@MacBook-Air-de-Matheus.local>
2022-05-17 00:51:13 -0300

    layout recuperacao de senha inicial

--- lib/telas/Recuperar_senha.dart
+++ lib/telas/Recuperar_senha.dart
@@ -15,39 +15,71 @@ class _ResetScreenState extends State<ResetScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Reset Password'),),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                  hintText: 'Email'
-              ),
-              onChanged: (value) {
-                setState(() {
-                  _controllerEmail.text = value.trim();
-                });
-              },
+      appBar: AppBar(
+        title: Text('Redefinir senha'),),
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right:  40
+        ),
+        color: Colors.white,
+        child: ListView(
+          children: [
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Image.asset("imagens/cadeado.png"),
             ),
+            SizedBox(
+              height: 20,
+            ),
+            Text("Esqueceu sua senha?",
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight .w500,
+            ),
+              textAlign: TextAlign.center,
+            ),
+            Text("Por favor, informe o E-mail associado a sua conta que enviaremos um link para o mesmo com as instruções para restauração da sua senha.",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+        TextField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+              hintText: 'Email'
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              RaisedButton(
-                child: Text('Send Request'),
-                onPressed: () {
-                  auth.sendPasswordResetEmail(email: _controllerEmail.text);
-                  Navigator.of(context).pop();
-                },
-                color: Theme.of(context).accentColor,
-              ),
-
-            ],
+          onChanged: (value) {
+            setState(() {
+              _controllerEmail.text = value.trim();
+            });
+          },
+        ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          RaisedButton(
+            child: Text('Send Request'),
+            onPressed: () {
+              auth.sendPasswordResetEmail(email: _controllerEmail.text);
+              Navigator.of(context).pop();
+            },
+            color: Theme.of(context).accentColor,
           ),
 
-        ],),
+        ],
+      ),
+          ],
+        ),
+      ),
+
     );
   }
 }
