9e547aa7c90da00e2f674d5a205a9516cb6a1a90
Matheus Neris <matheusneris@MacBook-Air-de-Matheus.local>
2022-05-28 15:08:51 -0300

    pesquisa na tela de guardas

--- lib/telas/ListagemGuardas.dart
+++ lib/telas/ListagemGuardas.dart
@@ -10,12 +10,38 @@ class Guardaslistagem extends StatefulWidget {
 }
 
 class _GuardaslistagemState extends State<Guardaslistagem> {
+  String Title = "Listagem dos Guardas";
+  bool  searchEnabled = false;
+
+  _switchSearchBarState(){
+    setState((){
+      searchEnabled = !searchEnabled;
+    }
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-        title: Text("Listagem dos Guardas"),
           backgroundColor: Color(0xFF092757),
+          title: !searchEnabled ? Text(Title) :
+          TextField(
+            style:  new TextStyle(
+              color:  Colors.white,
+            ),
+            decoration: new InputDecoration(
+              border: InputBorder.none,
+              prefixIcon: new Icon(Icons.search, color: Colors.white),
+              hintText: "Pesquisar",
+              hintStyle: new TextStyle(color: Colors.white)
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: Icon( searchEnabled? Icons.close : Icons.search),
+              onPressed: _switchSearchBarState,
+            )
+          ],
         ),
       body: Container(
         child:AbaContatos()
