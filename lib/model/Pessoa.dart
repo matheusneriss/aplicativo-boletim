2656b14a7684d729e5ef7cddb545019762aa6cc5
matheusneriss <matheusneris2011@gmail.com>
2022-06-17 15:43:20 -0300

    ajustes

--- lib/model/Pessoa.dart
+++ lib/model/Pessoa.dart
@@ -23,8 +23,47 @@ class Pessoa{
   String _celular = "";
   String _municipio = "";
   String _estadoNascimento = "";
+  String _excluido = "";
+
+  String get excluido => _excluido;
+
+  set excluido(String value) {
+    _excluido = value;
+  }
 
   Pessoa();
+
+  Map<String, dynamic> toMap(){
+    Map<String, dynamic> map = {
+      "Data de nascimento" : this.datadenasicmento,
+      "Cor" : this.cor,
+      "Nome" : this.nome,
+      "Logradouro" : this.logradouro,
+      "Bairro" : this.bairro,
+      "Numero" : this.numero,
+      "Cep" : this.cep,
+      "CPF" : this.cpf,
+      "RG" : this.rg,
+      "Uf" : this.Uf,
+      "Nome do pai" : this.nomedopai,
+      "Nome da mãe" : this.nomedamae,
+      "Naturalidade" : this.naturalidade,
+      "Profissao" : this.profissao,
+      "Uf do rg" : this.ufrg,
+      "CNH" : this.cnh,
+      "Número da cnh" : this.numcnh,
+      "Categoria da cnh" : this.catcnh,
+      "Exame CNH" : this.examecnh,
+      "Número de registro" : this.numregistro,
+      "Telefone" : this.telefone,
+      "Celular" : this.celular,
+      "Municipio" : this.municipio,
+      "Estado de Nascimento" : this.estadoNascimento,
+      "excluido": false,
+    };
+    return map;
+
+  }
 
   String get estadoNascimento => _estadoNascimento;
 
